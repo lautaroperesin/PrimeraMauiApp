b077a5cb205e841e2905b331916b0a898c67a56c
lautaroperesin <lautaroperesin@gmail.com>
2024-08-22 20:05:29 -0300

    Agregando más controles comunes

--- PrimeraMauiApp/Pages/ControlesComunesPage.xaml.cs
+++ PrimeraMauiApp/Pages/ControlesComunesPage.xaml.cs
@@ -1,9 +1,27 @@
+using System.Collections.ObjectModel;
+
 namespace PrimeraMauiApp.Pages;
 
 public partial class ControlesComunesPage : ContentPage
 {
-	public ControlesComunesPage()
+    public ObservableCollection<string> Frutas { get; set; }
+
+    public ControlesComunesPage()
 	{
 		InitializeComponent();
-	}
+        Frutas = new ObservableCollection<string>
+        {
+            "Apple",
+            "Banana",
+            "Orange",
+            "Grape",
+            "Pineapple",
+            "Strawberry",
+            "Mango",
+            "Peach",
+            "Cherry",
+            "Watermelon"
+        };
+        BindingContext = this;
+    }
 }
